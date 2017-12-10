58c52d1f1be9eb1eca40fe1177e3c32aa1ea9250
ChauMeng <ChauMeng@ChauMengNG>
2017-12-11 02:42:04 +0800

    menu with delete function

--- src/fooddeliverysystem/Menu.java
+++ src/fooddeliverysystem/Menu.java
@@ -3,17 +3,28 @@ package fooddeliverysystem;
 import javax.swing.JOptionPane;
 //import adt.*;
 import domain.domainMenu;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.DefaultListModel;
 
 public class Menu extends javax.swing.JFrame {
 
-   // public static ListInterface<domainMenu> MaintainMenu = new List<>();
+    // public static ListInterface<domainMenu> MaintainMenu = new List<>();
     private String FoodName = "Cheese Burger|Cheese Pizza|Chicken burger";
     private String FoodPrice = "5.00|9.00|7.00";
     private String[] FoodNameArr = FoodName.split("[|]");
     private String[] FoodPriceArr = FoodPrice.split("[|]");
+    private DefaultListModel dlmMenu=new DefaultListModel<>();
 
     public Menu() {
         initComponents();
+        initValue();
+    }
+
+    private void initValue() {
+        dlmMenu = (DefaultListModel) JListFoodMenu.getModel();
+        for (int i = 0; i < FoodNameArr.length; i++) {
+            dlmMenu.addElement(FoodNameArr[i]);
+        }
     }
 
     @SuppressWarnings("unchecked")
@@ -32,6 +43,7 @@ public class Menu extends javax.swing.JFrame {
         JTFPrice = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
         JUpdate = new javax.swing.JButton();
+        Bdelete = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMinimumSize(new java.awt.Dimension(800, 600));
@@ -59,11 +71,7 @@ public class Menu extends javax.swing.JFrame {
         jLabel3.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jLabel3.setText("Food Price  : ");
 
-        JListFoodMenu.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = FoodName.split("[|]");
-            public int getSize() { return strings.length; }
-            public String getElementAt(int i) { return strings[i]; }
-        });
+        JListFoodMenu.setModel(dlmMenu);
         JListFoodMenu.setMaximumSize(new java.awt.Dimension(350, 400));
         JListFoodMenu.setMinimumSize(new java.awt.Dimension(300, 400));
         JListFoodMenu.setPreferredSize(new java.awt.Dimension(350, 400));
@@ -85,6 +93,15 @@ public class Menu extends javax.swing.JFrame {
         JUpdate.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 JUpdateActionPerformed(evt);
+            }
+        });
+
+        Bdelete.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        Bdelete.setText("Delete");
+        Bdelete.setPreferredSize(new java.awt.Dimension(67, 31));
+        Bdelete.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BdeleteActionPerformed(evt);
             }
         });
 
@@ -111,18 +128,19 @@ public class Menu extends javax.swing.JFrame {
                         .addComponent(JTFPrice, javax.swing.GroupLayout.PREFERRED_SIZE, 125, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(6, 6, 6)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(Jadd)
-                            .addComponent(jLabel3))
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addGap(6, 6, 6)
+                                .addComponent(jLabel3)
                                 .addComponent(jtfFoodPrice, javax.swing.GroupLayout.PREFERRED_SIZE, 212, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(26, 26, 26)
-                                .addComponent(JUpdate, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                                .addGap(24, 24, 24)
+                                .addComponent(Jadd, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(JUpdate, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(Bdelete, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addContainerGap(230, Short.MAX_VALUE))))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(298, Short.MAX_VALUE)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -151,10 +169,11 @@ public class Menu extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
                     .addComponent(jtfFoodPrice, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(36, 36, 36)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(Jadd)
-                    .addComponent(JUpdate, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(35, 35, 35)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(JUpdate, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(Bdelete, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(Jadd, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(186, 186, 186))
         );
 
@@ -228,24 +247,36 @@ public class Menu extends javax.swing.JFrame {
         //MaintainMenu.replace(JListFoodMenu.getSelectedIndex(),dm);
         String food = JListFoodMenu.getSelectedValue();
         boolean found = false;
-        try{
-        double newPrice=Double.parseDouble(JTFPrice.getText());
-        for (int i = 0; i < FoodNameArr.length && !found; i++) {
-            if (FoodNameArr[i].equalsIgnoreCase(food)) {
-                FoodPriceArr[i] = String.format("%.2f",newPrice);
-                FoodPrice = "";
-                found=true;
+        try {
+            double newPrice = Double.parseDouble(JTFPrice.getText());
+            for (int i = 0; i < FoodNameArr.length && !found; i++) {
+                if (FoodNameArr[i].equalsIgnoreCase(food)) {
+                    FoodPriceArr[i] = String.format("%.2f", newPrice);
+                    FoodPrice = "";
+                    found = true;
+                }
             }
-        }
-        for (int i = 0; i < FoodPriceArr.length; i++) {
-            FoodPrice = FoodPrice.concat("|" + FoodPriceArr[i]);
-        }
-        FoodPrice = FoodPrice.substring(1);
-        JOptionPane.showMessageDialog(null, "Update successful", "Success", JOptionPane.INFORMATION_MESSAGE);
-        }catch (NumberFormatException ex) {
+            for (int i = 0; i < FoodPriceArr.length; i++) {
+                FoodPrice = FoodPrice.concat("|" + FoodPriceArr[i]);
+            }
+            FoodPrice = FoodPrice.substring(1);
+            JOptionPane.showMessageDialog(null, "Update successful", "Success", JOptionPane.INFORMATION_MESSAGE);
+        } catch (NumberFormatException ex) {
             JOptionPane.showMessageDialog(null, ex.toString(), "ERROR!", JOptionPane.ERROR_MESSAGE);
         }
     }//GEN-LAST:event_JUpdateActionPerformed
+
+    private void BdeleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BdeleteActionPerformed
+        // TODO add your handling code here:
+        
+        int food = JListFoodMenu.getSelectedIndex();
+        dlmMenu.removeElement(food);
+        JListFoodMenu.setModel(dlmMenu);
+
+        JOptionPane.showMessageDialog(null, "Delete successful", "Success", JOptionPane.INFORMATION_MESSAGE);
+
+
+    }//GEN-LAST:event_BdeleteActionPerformed
 
     public static void main(String args[]) {
 
@@ -257,6 +288,7 @@ public class Menu extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Bdelete;
     private javax.swing.JList<String> JListFoodMenu;
     private javax.swing.JTextField JTFPrice;
     private javax.swing.JButton JUpdate;
